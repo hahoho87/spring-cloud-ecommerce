a5f0bdd4292665a422eb9038b5ae24f5461c50b3
hahoho87 <bbaing4564@gmail.com>
2022-01-10 23:19:31 +0900

    Initial commit

---
+++ src/test/java/com/hahoho87/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hahoho87.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
